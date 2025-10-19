d48727e57333b2131ead6e94189db31526d8543c
meomeo1104 <10223053@student.vgu.edu.vn>
2025-10-20 00:37:31 +0700

    Update logger.js

--- functions/logger/logger.js
+++ functions/logger/logger.js
@@ -1,1 +1,10 @@
+const { createLogger, format, transports } = require('winston');
 
+module.exports = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`)
+  ),
+  transports: [new transports.Console()],
+});
